35328a6512bd18b785b81258430a08d716fb28b9
mayy-iztech <mayyiztech@gmail.com>
2024-10-29 12:23:06 +0200

    js with html

--- index.js
+++ index.js
@@ -1,72 +1,81 @@
-async function createOrderCart() {
-  const response = await fetch("http://192.168.1.136:8000/category/");
-  const data = await response.json();
-  const contentOrderDiv = document.getElementById("content-order");
 
-  const contentOrderCart = document.createElement("div");
-  contentOrderCart.className = "content-order-cart";
-  contentOrderCart.id = "content-order-cart";
-
-  for (const cat of data) {
-    const contentOrderInf = document.createElement("div");
-    contentOrderInf.className = "content-order-cart-inf";
-    contentOrderInf.id = "content-order-cart-inf";
-    contentOrderCart.appendChild(contentOrderInf);
-    const headerCart = document.createElement("div");
-    headerCart.className = "content-order-cart-head";
-    headerCart.id = "content-order-cart-head";
-    const spanHead = document.createElement("span");
-    spanHead.innerHTML = "orders---kitchen";
-    headerCart.appendChild(spanHead);
-    contentOrderInf.appendChild(headerCart);
-    const contentCartBody = document.createElement("div");
-    contentCartBody.className = "content-order-cart-body";
-    contentCartBody.id = "content-order-cart-body";
-
-    const spanName = document.createElement("span");
-    spanName.className = "content-order-cart-inf-span1";
-    spanName.innerHTML = cat.name;
-    contentCartBody.appendChild(spanName);
-
-    const spanPrice = document.createElement("span");
-    spanPrice.className = "content-order-cart-inf-span2";
-    spanPrice.innerHTML = cat.name + "$";
-    contentCartBody.appendChild(spanPrice);
-
-    contentOrderInf.appendChild(contentCartBody);
-  }
-
-  contentOrderDiv.appendChild(contentOrderCart);
-}
 async function CreateCategoryCart() {
-  const response = await fetch("http://192.168.1.136:8000/category/");
-  const data = await response.json();
-  const contentCart = document.getElementById("content-cat");
-  const contentCatCard = document.createElement("div");
-  contentCatCard.className = "content-cat-cart-inf";
-  contentCatCard.id = "content-cat-cart";
+    const response = await fetch("http://192.168.1.136:8000/category/");
+    
+    const data = await response.json();
+    const contentCat = document.getElementById("content-cat");
+    let i = 1;
+    for (const cat of data) {
+      const contentCatCart = document.createElement("div");
+      contentCatCart.className = `content-cat-cart content-cat-cart${i}`;
+      contentCatCart.id = "content-cat-cart";
+      contentCat.appendChild(contentCatCart);
 
-  for (const cat of data) {
-    const span = document.createElement("span");
-    span.className = "content-cat-cart-inf-span1";
-    span.innerHTML = cat.name;
-    const imgd = document.createElement("img");
-    formData.append("image", imgurl);
-    contentCatCard.appendChild(span);
-    contentCatCard.appendChild(imgd);
-    contentCart.appendChild(contentCatCard);
-    console.log(imgd.src);
- 
-  }   
-  const formData = new FormData();
-    const imgurl =
-      "C:/Users/IZtech-MShabaneh/Downloads/diagram-export-8-8-2024-2_23_24-PM.png";
-    formData.append("img_url", imgurl);
-    fetch("http://192.168.1.136:8000/category/", {
-      method: "POST",
-      body: formData,
-    }).then(() => console.log("success"));
+      const contentCatCartInf = document.createElement("div");
+      contentCatCartInf.className = "content-cat-cart-inf";
+      contentCatCart.appendChild(contentCatCartInf);
+
+      const span1 = document.createElement("span");
+      span1.className = "content-cat-cart-inf-span1";
+      span1.innerHTML = cat.name;
+
+      const span2 = document.createElement("span");
+      const data2 = await fetch(`http://192.168.1.136:8000/category/${cat.category_id}/items`);
+     
+      const items = await data2.json();
+      const length = items.length;
+
+      span2.className = "content-cat-cart-inf-span2";
+      span2.innerHTML = length === 1 ? `${length} item` : `${length} items`;
+
+      contentCatCartInf.appendChild(span1);
+      contentCatCartInf.appendChild(span2);
+
+      contentCatCart.addEventListener("click", () => CreateOrderCart(cat.category_id));
+      i++;
+    }
 }
 
-createOrderCart();
+async function CreateOrderCart(id) {
+    const contentOrder = document.getElementById("content-order");
+    const response = await fetch(`http://192.168.1.136:8000/category/${id}/items`);
+    contentOrder.innerHTML = "";
+    const data = await response.json();
+    for (const item of data) {
+      const contentOrderCard = document.createElement("div");
+      contentOrderCard.className = "content-order-cart";
+      contentOrder.appendChild(contentOrderCard);
+
+      const contentBorder = document.createElement("div");
+      contentBorder.className = "content-order-border";
+      contentOrderCard.appendChild(contentBorder);
+
+      const contentOrderInf = document.createElement("div");
+      contentOrderInf.className = "content-order-cart-inf";
+      contentOrderCard.appendChild(contentOrderInf);
+
+      const contOrderHead = document.createElement("div");
+      contOrderHead.className = "content-order-cart-head";
+      contentOrderInf.appendChild(contOrderHead);
+
+      const spanHead = document.createElement("span");
+      spanHead.innerHTML = "Orders ---- Kitchen";
+      contOrderHead.appendChild(spanHead);
+
+      const contOrderBody = document.createElement("div");
+      contOrderBody.className = "content-order-cart-body";
+      const spanOrderName = document.createElement("span");
+      spanOrderName.className = "content-order-cart-inf-span1";
+      spanOrderName.innerHTML = item.name;
+      contOrderBody.appendChild(spanOrderName);
+
+      const spanOrderPrice = document.createElement("span");
+      spanOrderPrice.className = "content-order-cart-inf-span2";
+      spanOrderPrice.innerHTML = `${item.price} $`;
+      contOrderBody.appendChild(spanOrderPrice);
+      contentOrderInf.appendChild(contOrderBody);
+    }
+ 
+}
+
 CreateCategoryCart();
